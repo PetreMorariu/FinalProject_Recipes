7ad36298ecac977db946f4aa01558f13c1eb9719
pmorariu <morariupetru@yahoo.com>
2025-04-03 00:09:50 +0300

    create a superuser, and create the Recipe model and register that in the admin/ page

--- recipes/views.py
+++ recipes/views.py
@@ -1,33 +1,9 @@
 from django.shortcuts import render
-
-recipes = [
-    {
-       'author': 'PetreM',
-       'title': 'Recipe post 1',
-       'content' : 'First recipe content',
-       'date_posted': 'April 2, 2025',
-       'cook_time': '2 hours'
-
-    },
-    {
-       'author': 'Jane Doe',
-       'title': 'Recipe post 2',
-       'content' : 'Second recipe content',
-       'date_posted': 'April 1, 2025',
-       'cook_time': '1 hour'
-    },
-    {
-       'author': 'PetreM',
-       'title': 'Recipe post 3',
-       'content' : 'Third recipe content',
-       'date_posted': 'April 3, 2025',
-       'cook_time': '3 hours'
-    }
-]
+from .models import Recipe
 
 def home(request):
     context = {
-        'recipes': recipes
+        'recipes': Recipe.objects.all()
     }
     return render(request, 'recipes/home.html',context)
 
