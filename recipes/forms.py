487708c2338ee9950bc6e3f3673f6659da61e189
pmorariu <morariupetru@yahoo.com>
2025-04-09 22:04:58 +0300

    small changest on the edit/detail view for a recipe

--- recipes/forms.py
+++ recipes/forms.py
@@ -4,4 +4,4 @@ from .models import Recipe
 class RecipeForm(forms.ModelForm):
     class Meta:
         model = Recipe
-        fields = ['title', 'content', 'date_created','cook_time','prep_time']
+        fields = ['title', 'content', 'date_created','prep_time','cook_time']
--- recipes/urls.py
+++ recipes/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.home, name='recipes-home'),
     path('about/', views.about, name='recipes-about'),
     path('recipe/add/', views.add_recipe, name='add-recipes'),
-    path('recipe/edit/<int:recipe_id>/', views.edit_recipe, name='edit-recipes'),
+    path('recipe/<int:recipe_id>/edit', views.edit_recipe, name='edit-recipes'),
     path('recipe/<int:recipe_id>/', views.detail_view_recipe, name='detail-recipes'),
 ]
