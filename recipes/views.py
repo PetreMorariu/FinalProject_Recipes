4242c91831d10ab0d48b33a4b0c1478e61294470
pmorariu <morariupetru@yahoo.com>
2025-04-09 09:55:48 +0300

    Changed Recipe model and form to have cook_time and prep_time and will display in home.html Total Coking Time by attding the cook_time and prep_time

--- recipes/views.py
+++ recipes/views.py
@@ -5,10 +5,28 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
 def home(request):
+    recipes = Recipe.objects.all()
+
+    for recipe in recipes:
+        total_time = format_total_cook_time(recipe.prep_time + recipe.cook_time)
+        recipe.total_time = total_time  # Attach to each recipe instance
+
     context = {
-        'recipes': Recipe.objects.all()
+        'recipes': recipes
     }
+
     return render(request, 'recipes/home.html',context)
+
+# create function that will format the Total cooking time
+def format_total_cook_time(duration):
+    hours = duration // 60
+    minutes = duration % 60
+    minute_descriptor  = "minute" if minutes == 1 else "minutes"
+    hour_descriptor  = "hour" if hours == 1 else "hours"
+
+    return "{hours} {hour_descriptor} {minutes} {minute_descriptor}".format(hours=hours, minutes=minutes,
+                                                                            minute_descriptor=minute_descriptor,
+                                                                            hour_descriptor=hour_descriptor)
 
 @login_required
 def add_recipe(request):
