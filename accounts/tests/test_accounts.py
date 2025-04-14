1d6b09fc04d21c25993f6def754de44b97ce0ff3
pmorariu <morariupetru@yahoo.com>
2025-04-14 23:03:40 +0300

    started the unit test for accounts and for recipes app v3

--- accounts/tests/test_accounts.py
+++ accounts/tests/test_accounts.py
@@ -4,9 +4,8 @@ from django.contrib.auth import get_user_model
 
 @pytest.mark.django_db
 def test_create_user():
-    user = get_user_model().objects.create_user(username="user1", password="testing321")
+    user = get_user_model().objects.create_user(username="user1", password="testing321",email="user1@company.com")
     assert user is not None
     assert user.username == "user1"
+    assert user.email == "user1@company.com"
     assert len(get_user_model().objects.all()) == 1
-    user = get_user_model().objects.create_user(username="user2", password="testing321")
-    assert len(get_user_model().objects.all()) == 2
