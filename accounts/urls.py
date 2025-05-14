e2d58cb984cc46b1cb6c6e25cd7120b3e42c20c9
pmorariu <morariupetru@yahoo.com>
2025-05-14 09:31:16 +0300

    Clean out code

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,6 +8,5 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
     path('register/', views.register, name='register'),
     path('profile/', views.view_profile, name='view_profile'),
-    path('profile/edit/', views.edit_profile, name='edit_profile'),
-    # path('logout/', LogoutView.as_view(), name='logout'),
+    path('profile/edit/', views.edit_profile, name='edit_profile')
 ]
