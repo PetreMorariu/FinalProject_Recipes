2955c88b2a249f7b331c08b335ee773fcac712f3
pmorariu <morariupetru@yahoo.com>
2025-05-16 10:20:19 +0300

    commented out the date_created for unit test as I got an warning

--- recipes/models.py
+++ recipes/models.py
@@ -6,7 +6,7 @@ class Recipe(models.Model):
     title = models.CharField(max_length=100)
     ingredients = models.TextField()
     cooking_steps = models.TextField()
-    date_created = models.DateTimeField(default=timezone.now)
+    date_created = models.DateTimeField(default=timezone.now())
     cook_time = models.IntegerField()
     prep_time = models.IntegerField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)  #create a one-many connection;If User will be deleted, then his recipes will be deleted as well
@@ -20,7 +20,7 @@ class Comment(models.Model):
     recipe = models.ForeignKey(Recipe, related_name='comments', on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField()
-    data_created_comment = models.DateTimeField(default=timezone.now)
+    data_created_comment = models.DateTimeField(default=timezone.now())
 
     def __str__(self):
         return f'{self.text}'
