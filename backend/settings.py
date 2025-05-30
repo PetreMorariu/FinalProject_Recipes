f09744960e319ef6b717fc054f63c863adca666d
pmorariu <morariupetru@yahoo.com>
2025-05-30 10:58:49 +0300

    Updated setting

--- backend/settings.py
+++ backend/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x6r=^s!m%_zhfd6^@)bgmzn2t(wnj)tc463g^d&p%3m#%8qbuy'
+# SECRET_KEY = 'django-insecure-x6r=^s!m%_zhfd6^@)bgmzn2t(wnj)tc463g^d&p%3m#%8qbuy'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["myrecipesfinalv-203c42bf8d0a.herokuapp.com"]
 
 
 # Application definition
