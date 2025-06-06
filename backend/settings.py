c45cd8829b05e3d914a34012781052faecce5bd2
pmorariu <morariupetru@yahoo.com>
2025-06-06 10:59:38 +0300

    reverted the secret key value

--- backend/settings.py
+++ backend/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-x6r=^s!m%_zhfd6^@)bgmzn2t(wnj)tc463g^d&p%3m#%8qbuy'
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-x6r=^s!m%_zhfd6^@)bgmzn2t(wnj)tc463g^d&p%3m#%8qbuy'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
